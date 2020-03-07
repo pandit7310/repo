4a5724cc2d6692c7b34f686f5ffcbb5f6d0599dd
Training <Training@TRD-316.mshome.net>
2020-03-07 15:34:02 +0530

    new branch commit

--- GitPlayground/src/NewBranch1.java
+++ GitPlayground/src/NewBranch1.java
@@ -1,9 +1,9 @@
 
-public class NewBranch {
+public class NewBranch1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello this is a new branch");
+		
 	}
 
 }
